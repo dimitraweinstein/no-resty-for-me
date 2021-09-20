01aff7a6a0812db98d2187c1c2f00299a537fd1d
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-09-19 17:11:17 -0700

    updated container to with ternary in order to have proper render and display controls, update to test is needed

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,21 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Controls = ({ url, onChange }) => (
-  <>
-    <input
-      id="input"
-      type="text"
-      name="url"
-      value={url}
-      onChange={onChange}
-    />
-  </>
-);
+const Controls = ({ url, body, onChange, onSubmit }) => {
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          id="input"
+          placeholder="URL"
+          type="text"
+          name="url"
+          value={url}
+          onChange={onChange}
+        />
+        <button type="radio">Get</button>
+        <button type="radio">Put</button>
+        <button type="radio">Post</button>
+        <button type="radio">Patch</button>
+        <button type="radio">Delete</button>
+        <button
+          aria-label="request"
+          type="submit">Go!</button>
+        <textarea
+          placeholder="Raw JSON Body"
+          name="body"
+          value={body}
+          rows="10"
+          cols="50"
+          onChange={onChange}
+        ></textarea>
+      </form>
+    </>
+  );
+};
 
 Controls.propTypes = {
   url: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired
+  body: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
 };
 
 export default Controls;
--- src/containers/RestyContainer.jsx
+++ src/containers/RestyContainer.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Controls from '../components/Controls';
 
 
-const RestyContainer = () => {
-  return <h1>Loading...</h1>;
-};
+export default class RestyContainer extends Component {
+  state = {
+    loading: true,
+    url: '',
+    body: '',
+  }
 
-export default RestyContainer;
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit = () => {
+  
+  }
+
+  render() {
+    const { loading, url, body } = this.state;
+    
+    return (
+      <>
+        <Controls
+          url={url}
+          body={body}
+          onChange={this.handleChange}
+          onSubmit={this.handleSubmit}
+        />
+
+        { loading ? (<img
+          // eslint-disable-next-line max-len
+          src="https://icon-library.com/images/ajax-loading-icon/ajax-loading-icon-2.jpg"
+          alt="loading spinner" />) : (
+          <>
+            <Display />
+          </>
+        )
+        }
+      </>
+    );
+  }
+}
